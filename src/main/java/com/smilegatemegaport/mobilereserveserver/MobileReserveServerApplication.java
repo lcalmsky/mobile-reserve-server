7a75145e92fb42d3a56fff3a5f81af972d42b34d
minhoe <minhoe1122@motov.co.kr>
2021-05-15 21:49:38 +0900

    Initialize Project

---
+++ src/main/java/com/smilegatemegaport/mobilereserveserver/MobileReserveServerApplication.java
@@ -0,0 +1,13 @@
+package com.smilegatemegaport.mobilereserveserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileReserveServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileReserveServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smilegatemegaport/mobilereserveserver/MobileReserveServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smilegatemegaport.mobilereserveserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileReserveServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
